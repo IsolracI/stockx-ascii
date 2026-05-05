50bd29f70bc72774fb7c271a236383425a412756
|solrac| <gabrieldiacich@gmail.com>
2026-05-06 00:36:43 +0200

    feat(Max): diseñar clase Max para pasar la decimotercera historia de usuario

---
+++ src/main/java/teamrocket/criteria/Max.java
@@ -0,0 +1,39 @@
+package teamrocket.criteria;
+
+import teamrocket.item.Item;
+import teamrocket.item.Offer;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Max implements Criteria {
+    private final Criteria criteria;
+    private final Criteria otherCriteria;
+
+    public Max(Criteria criteria, Criteria otherCriteria) {
+        this.criteria = criteria;
+        this.otherCriteria = otherCriteria;
+    }
+
+    @Override
+    public List<Offer> checkCriteria(Item item) {
+        List<Offer> CriteriaList = criteria.checkCriteria(item);
+        List<Offer> otherCriteriaList = otherCriteria.checkCriteria(item);
+
+        List<Offer> filteredList = new ArrayList<>();
+
+        for (Offer offer : CriteriaList) {
+            for (Offer otherOffer : otherCriteriaList) {
+                if (offer == otherOffer) {
+                    filteredList.add(offer);
+                    break;
+                }
+            }
+        }
+
+        filteredList.sort(Comparator.comparingInt(Offer::value).reversed());
+
+        return filteredList;
+    }
+}
